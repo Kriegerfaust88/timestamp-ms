3b951f74f5da90ae9c01d6e61873cb4107bd72b5
Daniel Schroeder <dschroeder101@gmail.com>
2016-12-07 17:49:20 +0000

    heroku setup

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
     res.send('Hello world!')
 })
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('Listening on port 8080')
 })
