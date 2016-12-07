6e904087bc0b733e8ab44adabee0f2cb153a5e71
Daniel Schroeder <dschroeder101@gmail.com>
2016-12-07 18:54:49 +0000

    Routing based on URL parameter

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express')
+var url = require('url')
 var app = express()
 
 app.get('/', function(req, res) {
-    res.send('Hello world!')
+    res.send('Enter date as a URL parameter to receive a timestamp back eg. https://api-projects-kriegerfaust.c9users.io/December%2015,%202015')
+})
+
+app.get('/:input', function(req, res) {
+    var input = req.params.input;
+    res.send(input);
 })
 
 app.listen(process.env.PORT || 8080, function() {
-    console.log('Listening on port 8080')
+    console.log('Listening...');
 })
